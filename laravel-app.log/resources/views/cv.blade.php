104b517b4cc91f85d76aa6802e841f30171eb06a
Ihor <93588430+IhorDovhanych@users.noreply.github.com>
2023-03-07 15:57:46 +0200

    app

---
+++ laravel-app.log/resources/views/cv.blade.php
@@ -0,0 +1,43 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/app.css" type="text/css">
+</head>
+<body>
+<div class="container">
+    <header>
+        <h2> Welcome to {{$name}}'s cv page</h2>
+    </header>
+    <div class="cv-main-section">
+        <section>
+            <h3>E D U C A T I O N</h3>
+            <p>Uzhhorod National University,</p>
+            <p>Uzhhorod, Ukraine</p>
+            <p>Faculty of Maths and Digital</p>
+            <p>Technologies System analysis</p>
+            <p>Bachelor ’ s degree - 2021 - for now</p>
+        </section>
+        <section>
+            <h3>S K I L L S</h3>
+            <ul>
+                <li>HTML, CSS, JS</li>
+                <li>VueJS (Vue CLI, Vuex)</li>
+                <li>Angular</li>
+                <li>C#</li>
+                <li>Python</li>
+                <li>PHP</li>
+                <li>SQL/MySQL</li>
+                <li>Figma, Photoshop, Illustrator</li>
+                <li>OOP, Design paterns</li>
+            </ul>
+        </section>
+    </div>
+</div>
+</body>
+</html>
